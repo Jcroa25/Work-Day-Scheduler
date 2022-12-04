e0f583b70f86a9ac4564951168c0f70cb41d9f46
JuanCarlos Roa Jr <roajrjuancarlos@hotmail.com>
2022-12-04 16:03:05 -0500

    added hour tracker function

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,13 @@ $(document).ready(function () {
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
     })
-
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+    $("#hour6 .description").val(localStorage.getItem("hour6"));
+    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -24,10 +30,33 @@ $(document).ready(function () {
     $("#hour22 .description").val(localStorage.getItem("hour22"));
     $("#hour23 .description").val(localStorage.getItem("hour23"));
     $("#hour24 .description").val(localStorage.getItem("hour24"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
-    $("#hour6 .description").val(localStorage.getItem("hour6"));
-    $("#hour7 .description").val(localStorage.getItem("hour7"));})
+
+
+    function hourTracker() {
+        var currentHour = moment().hour();
+
+       
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+
+            
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker();
+})
